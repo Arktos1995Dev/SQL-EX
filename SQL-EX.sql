c33794f5e0bbf3f7d6e2809c488c8c3b1dfb81f4
Nikita <Arktos1995@ya.ru>
2022-11-25 12:05:19 +0300

    Update SQL-EX.sql

--- SQL-EX.sql
+++ SQL-EX.sql
@@ -1,23 +1,6 @@
-SQL-EX
+Solutions to SQL-EX.ru excercises
 
 #32
-/*Select
- country,
- cast(avg((bore*bore*bore)/2) AS NUMERIC(6,2)) as mv
-from ( select cast (bore AS NUMERIC(6,2)) as bore, country, class from classes) c
-join (
- select ship as class from outcomes
- union all
- select class from ships) s on c.class=s.class
-group by country
-
-SELECT country, cast(avg((power(bore,3)/2)) AS numeric(6,2)) AS weight 
-FROM (SELECT country, classes.class, bore, name FROM classes LEFT JOIN ships ON classes.class=ships.class 
-	UNION ALL 
-	SELECT DISTINCT country, class, bore, ship FROM classes t1 LEFT JOIN outcomes t2 ON t1.class=t2.ship 
-	WHERE ship=class and ship not in (SELECT name FROM ships) 
-) a WHERE name IS NOT NULL GROUP BY country*/
-
 #1 вариант
 SELECT country, cast(avg((power(bore,3)/2)) AS numeric(6,2)) AS weight 
 FROM (SELECT country, classes.class, bore, name FROM classes JOIN ships ON classes.class=ships.class 
@@ -41,7 +24,7 @@ SELECT c.country, cast(AVG(POWER(c.bore, 3) / 2) as NUMERIC(6, 2)) AS weight
     ON s.class = c.class
  GROUP BY c.country
 
- #35
+#35
 SELECT model, type
 FROM Product
 WHERE model not LIKE '%[^A-Z]%'
@@ -304,3 +287,242 @@ where date < '20010415'
 group by point
 )x
 group by point
+
+
+#63
+Определить имена разных пассажиров, когда-либо летевших на одном и том же месте более одного раза.
+
+SELECT name
+FROM passenger
+WHERE id_psg IN 
+(SELECT distinct t.id_psg
+FROM Pass_in_trip t  
+GROUP by t.id_psg, t.place
+HAVING  
+COUNT(*) > 1)
+
+
+#65
+Пронумеровать уникальные пары {maker, type} из Product, упорядочив их следующим образом:
+- имя производителя (maker) по возрастанию;
+- тип продукта (type) в порядке PC, Laptop, Printer.
+Если некий производитель выпускает несколько типов продукции, то выводить его имя только в первой строке;
+остальные строки для ЭТОГО производителя должны содержать пустую строку символов ('').
+Функция ROW_NUMBER http://www.sql-tutorial.ru/ru/book_row_number_function.html
+
+select num, case when num2 = 1 then maker end as maker, type
+from (
+Select 
+row_number () over(order by maker, type_no) num,
+row_number () over(partition by maker order by maker, type_no) num2,
+maker, type
+from (select distinct maker, type, 
+case when type='PC' then 1 
+when type='Laptop' then 2 
+when type='Printer' then 3 end as type_no
+from Product)x
+)x2
+
+
+
+Select
+count(trip_no) count,
+DATE(time_out) date
+from Trip
+where town_from='Rostov'
+--and time_out>='20030401'
+--and time_out<='20030407'
+group by DATE(time_out)
+
+#65
+Для всех дней в интервале с 01/04/2003 по 07/04/2003 определить число рейсов из Rostov.
+Вывод: дата, количество рейсов
+
+WITH t1 AS (
+SELECT CAST('2003-04-01' AS DATETIME) 'date' 
+     UNION ALL
+     SELECT DATEADD(dd, 1, t.date) 
+       FROM t1 t
+where t.date <'2003-04-07'
+)
+
+SELECT tt.date, (SELECT COUNT(1) FROM (SELECT DISTINCT t.trip_no
+FROM pass_in_trip pip, trip t
+WHERE pip.trip_no = t.trip_no
+AND t.town_from = 'rostov'
+AND tt.date = pip.date) trips )Qty
+FROM t1 tt
+
+#65
+Найти количество маршрутов, которые обслуживаются наибольшим числом рейсов.
+Замечания.
+1) A - B и B - A считать РАЗНЫМИ маршрутами.
+2) Использовать только таблицу Trip
+
+with t1 as 
+(
+Select
+ count(trip_no) qnt,
+town_from, town_to
+from trip
+group by town_from, town_to
+),
+t2 as (
+select max(qnt) mqnt from t1
+)
+select count(*) from t1
+join t2 on t2.mqnt=t1.qnt
+
+#68
+Найти количество маршрутов, которые обслуживаются наибольшим числом рейсов.
+Замечания.
+1) A - B и B - A считать ОДНИМ И ТЕМ ЖЕ маршрутом.
+2) Использовать только таблицу Trip
+with table1 as (
+select CASE WHEN town_from<=town_to 
+THEN town_from+town_to 
+ELSE town_to+town_from END ab,
+count(*) ct from trip 
+group by CASE WHEN town_from<=town_to 
+THEN town_from+town_to 
+ELSE town_to+town_from END ) 
+
+select count(*) from table1 where ct=(select max(ct) from table1)
+
+
+
+#66
+По таблицам Income и Outcome для каждого пункта приема найти остатки денежных средств на конец каждого дня,
+в который выполнялись операции по приходу и/или расходу на данном пункте.
+Учесть при этом, что деньги не изымаются, а остатки/задолженность переходят на следующий день.
+Вывод: пункт приема, день в формате "dd/mm/yyyy", остатки/задолженность на конец этого дня.
+Функция CONVERT http://www.sql-tutorial.ru/ru/book_type_conversion_and_cast_function/page2.html
+
+with t1 as (
+Select 
+point, date, sum(inc) inc
+from income i
+group by point, date
+union all
+Select 
+point, date, sum(-out) inc
+from outcome i
+group by point, date
+)
+Select 
+ point, CONVERT(char(25), date,103) date,
+(SELECT SUM(inc) 
+   FROM t1  x2
+   WHERE point = t1 .point AND date <= t1 .date) run_tot
+from t1
+group by point, date
+
+
+
+#71
+Найти тех производителей ПК, все модели ПК которых имеются в таблице PC.
+Реляционное деление http://www.sql-tutorial.ru/ru/book_relational_division.html
+
+Select
+ maker
+from product p
+where type='PC'
+group by maker
+having count(DISTINCT model) = (SELECT COUNT(DISTINCT product.model) FROM Product join pc on product.model=pc.model where type='PC' and p.maker=Product.maker)
+
+
+#72
+Среди тех, кто пользуется услугами только какой-нибудь одной компании, определить имена разных пассажиров, летавших чаще других.
+Вывести: имя пассажира и число полетов.
+
+with t1 as (
+	Select 
+ 		pit.id_psg
+	from pass_in_trip pit
+	join trip tr on pit.trip_no=tr.trip_no
+	group by pit.id_psg
+	having count(distinct tr.id_comp) =1
+)
+, t2 as (
+	select p.id_psg, count(*) qnt 
+	from pass_in_trip p
+	join t1 on t1.id_psg=p.id_psg
+	group by p.id_psg
+)
+, t3 as (
+	select max(qnt) as mqnt from t2
+)
+select 
+	name, 
+	t2.qnt 
+from passenger ps
+join t2 on t2.id_psg = ps.id_psg
+join t3 on t3.mqnt = t2.qnt
+
+
+
+#73
+Для каждой страны определить сражения, в которых не участвовали корабли данной страны.
+Вывод: страна, сражение
+
+with t1 as (select distinct country, name as battle from classes c, battles b)
+select t1.* from t1 left join 
+(
+Select country, name, battle 
+from Classes c
+join (
+	select ship as class, ship as name, o.battle from Outcomes o where ship not in (select name from ships)
+	union all
+	select class, name, o.battle from Outcomes o join ships s on 
+	o.ship=s.name) a on a.class=c.class) a on t1.country=a.country
+and t1.battle=a.battle
+where a.battle is null
+
+
+#74
+Вывести все классы кораблей России (Russia). Если в базе данных нет классов кораблей России, вывести классы для всех имеющихся в БД стран.
+Вывод: страна, класс
+
+#1 вариант
+Select
+ country, class
+from Classes c
+where case when exists (select country from classes where country='Russia') then 'Russia' 
+else country end = country
+
+#2 вариант
+SELECT  country, class
+FROM classes
+WHERE country = ALL (SELECT   country
+FROM classes
+WHERE country = 'Russia')
+
+
+#75
+Для тех производителей, у которых есть продукты с известной ценой хотя бы в одной из таблиц Laptop, PC, Printer найти максимальные цены на каждый из типов продукции.
+Вывод: maker, максимальная цена на ноутбуки, максимальная цена на ПК, максимальная цена на принтеры.
+Для отсутствующих продуктов/цен использовать NULL.
+
+with p as (select maker, type, model from product),
+price_t as (
+  Select
+   maker, p.type, price as mprice
+  from p join pc on pc.model=p.model and p.type='PC'
+union all
+  Select
+   maker, p.type, price as mprice
+  from p join Laptop pc on pc.model=p.model and p.type='Laptop'
+union all
+  Select
+   maker, p.type, price as mprice
+  from p join Printer pc on pc.model=p.model and p.type='Printer'),
+t1 as (
+  select maker, case when type='Laptop' then mprice end as mprice_l,
+  case when type='PC' then mprice end as mprice_pc,
+  case when type='Printer' then mprice end as mprice_pr
+  from price_t)
+select maker, max(mprice_l) mprice_l, max(mprice_pc) mprice_pc, max(mprice_pr) mprice_pr
+from t1
+group by maker
+having max(mprice_l) is not null or max(mprice_pc) is not null or max(mprice_pr) is not null
+
